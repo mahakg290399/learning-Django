348630099dff9c8475bce2b0a2af2a465d580a6d
Mahak Goyal <mahakg290399@gmail.com>
2022-09-17 18:51:07 +0530

    amazed by authentication process in Django

--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
     return render(request, 'home/welcome.html',{'today': datetime.today()})
 
+@login_required(login_url='/admin')
 def authorize(request):
     return render(request, 'home/authorize.html',{})
