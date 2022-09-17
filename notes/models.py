8c03ba49a1e5685655925555f1cc09c12bce87cf
Mahak Goyal <mahakg290399@gmail.com>
2022-09-17 22:35:11 +0530

    Implemented my first View

---
+++ notes/models.py
@@ -0,0 +1,11 @@
+from pyexpat import model
+from turtle import title
+from django.db import models
+
+# Create your models here.
+
+class Notes(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
