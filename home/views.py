3d061ed83f3f58ed2bc8e49b5b669f3cf7badf84
Mahak Goyal <mahakg290399@gmail.com>
2022-09-17 18:16:45 +0530

    Some minutes learning

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
+import imp
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from datetime import datetime
+def home(request):
+    print(request)
+    return render(request, 'home/welcome.html',{'today': datetime.today()})
