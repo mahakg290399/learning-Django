f59493a8d872eaae7d6e27f5160f454fd1085c6c
Mahak Goyal <mahakg290399@gmail.com>
2022-09-18 22:42:56 +0530

    Learnt Class based view

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
+    path('home', views.HomeView.as_view()),
     path('authorise', views.authorize)
 ]
 
